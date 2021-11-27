ba7f7688628b5b24e0f7a3fdba8182a155726537
Raveen Kumar <raveen.srk.kumar@gmail.com>
2021-11-27 22:10:10 +0530

    Sync commit from raveensrk@my-fedora

--- stow/.local/scripts/my-scripts-personal/dl_music.bash
+++ stow/.local/scripts/my-scripts-personal/dl_music.bash
@@ -2,15 +2,19 @@
 
 # Download music from youtube
 
-pushd "/mnt/HDD-1TB/Music Collections/Music"
+# pushd "/mnt/HDD-1TB/Music Collections/Music"
 
-youtube-dl \
+yt-dlp \
     --extract-audio \
     --audio-format mp3 \
-    --audio-quality 0 "$1" \
-    --add-metadata \
+    --audio-quality 0 \
     -i \
     --embed-thumbnail \
     --metadata-from-title  "%(artist)s  -  %(title)s" \
+    "$1" \
+
+ #        --external-downloader axel \
+
+
     
     
