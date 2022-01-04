e5232e9859a735c2cf886783e3f5a1501e95ba88
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-01-04 20:26:05 +0530

    Sync commit from raveensrk@DESKTOP-67174F4

--- install.bash
+++ install.bash
@@ -4,16 +4,9 @@ set -e
 
 script_dir=$(dirname "$0")
 
-my_bash_aliases_link () {
-    local name=$(basename "$1")
-    local dest_link_path="$HOME/.my_bash_aliases/$name"
-    [ -L "$dest_link_path" ] && rm "$dest_link_path"
-    ln -s $(realpath "$1") "$dest_link_path"
-}
-
 pushd $script_dir
 
-stow stow
+stow -R stow -t ~/ --no-folding
 
 popd
 
