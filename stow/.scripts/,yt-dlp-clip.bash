cc6ee2f8713dfe668a372463643077fdef0a987a
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-05-16 02:34:10 +0530

    Update youtube clipper script

--- stow/.scripts/,yt-dlp-clip.bash
+++ stow/.scripts/,yt-dlp-clip.bash
@@ -8,11 +8,11 @@ while [ "$1" ]; do
             ;;
         --ss)
             shift
-            ss="-ss $1"
+            ss="$1"
             ;;
         --to)
             shift
-            to="-to $1"
+            to="$1"
             ;;
         --help|-h)
             help
@@ -31,5 +31,20 @@ done
 # yt-dlp -f "bv+ba/b" "$url" --get-filename --restrict-filenames --merge-output-format mp4 -o %(title)s_%(id)s.%(ext)s --ppa "ffmpeg_i1:$ss $to"
 file_name=$(yt-dlp --no-playlist -f "bv+ba/b" "$url" --get-filename --restrict-filenames --merge-output-format mp4 -o %\(title\)s_%\(id\)s.%\(ext\)s)
 yt-dlp --no-playlist -f "bv+ba/b" "$url" --restrict-filenames --merge-output-format mp4 -o %\(title\)s_%\(id\)s.%\(ext\)s
-ffmpeg $ss $to -i "$file_name" -preset veryslow "clip_${file_name}"
-open "clip_${file_name}"
+
+if [ "$ss" = "" ] && [ "$to" = "" ]; then
+    
+else
+clip_time=$(echo "clip_ss_${ss}_to_${to}" | tr ":" "-")
+echo $clip_time
+out="${clip_time}_${file_name}"
+echo $out
+ss="-ss $ss"
+to="-to $to"
+fi
+
+
+ffmpeg $ss $to -i "$file_name" -preset veryfast "$out"
+
+open "$out"
+
