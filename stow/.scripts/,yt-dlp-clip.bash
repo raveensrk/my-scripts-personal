9e052e205a6ea3cd12de66dcb655277dde11831d
Raveen Kumar <raveen.srk.kumar@gmail.com>
2022-06-10 02:48:46 +0530

    Youtube downloader script update

--- stow/.scripts/,yt-dlp-clip.bash
+++ stow/.scripts/,yt-dlp-clip.bash
@@ -44,38 +44,38 @@ file_name=$(yt-dlp --no-playlist -f "bv+ba/b" "$url" --get-filename --restrict-f
 
 yt-dlp --write-subs --write-auto-subs --no-playlist -f "bv+ba/b" "$url" --restrict-filenames --merge-output-format mp4 -o %\(title\)s_%\(id\)s.%\(ext\)s
 
-if [ "$ss" = "" ] && [ "$to" = "" ]; then
-    orig_file_name="orig_${file_name}"
-    mv "${file_name}" "$orig_file_name"
-
-    if [ "$scale" != "" ]; then
-        tmp="tmp.mp4"
-        mv "$orig_file_name" "$tmp" 
-        ffmpeg -i "$tmp" -preset veryfast $scale "$orig_file_name"
-        rm "$tmp"
-    fi
-
-    if [ ! "$mp3_only" = "1" ]; then
-        out="${file_name}.mp4"
-        ffmpeg -i "$orig_file_name" -preset veryfast "$out"
-    fi
-
-    if [ "$mp3" = "1" ] || [ "$mp3_only" = "1" ]; then
-        out2="${file_name}.mp3"
-        ffmpeg -i "$orig_file_name" -preset veryfast "$out2"
-    fi
-
-    rm "${orig_file_name}"
-else
-    clip_time=$(echo "clip_ss_${ss}_to_${to}" | tr ":" "-")
-    echo $clip_time
-    out="${clip_time}_${file_name}"
-    echo $out
+if [ ! "$ss" = "" ] && [ ! "$to" = "" ]; then
     ss="-ss $ss"
     to="-to $to"
-    ffmpeg $ss $to -i "$file_name" -preset veryfast "$out"
 fi
 
+orig_file_name="orig_${file_name}"
+mv "${file_name}" "$orig_file_name"
+
+if [ "$scale" != "" ]; then
+    tmp="tmp.mp4"
+    mv "$orig_file_name" "$tmp" 
+    ffmpeg -i "$tmp" -preset veryfast $scale "$orig_file_name"
+    rm "$tmp"
+fi
+
+if [ ! "$mp3_only" = "1" ]; then
+    out="${file_name}.mp4"
+    ffmpeg -i "$orig_file_name" -preset veryfast "$out"
+fi
+
+if [ "$mp3" = "1" ] || [ "$mp3_only" = "1" ]; then
+    out2="${file_name}.mp3"
+    ffmpeg -i "$orig_file_name" -preset veryfast "$out2"
+fi
+
+if [ ! "$ss" = "" ] && [ ! "$to" = "" ]; then
+    clip_time=$(echo "clip_ss_${ss}_to_${to}" | tr ":" "-")
+    out="${clip_time}_${orig_file_name}"
+    ffmpeg $ss $to -i "$orig_file_name" -preset veryfast "$out"
+fi
+
+rm "${orig_file_name}"
 
 if [ ! "$mp3_only" = "1" ]; then
     mpv "$out"
